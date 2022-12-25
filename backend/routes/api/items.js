a2a76a669254895d0e1aacef7aa7491971a9648d
Justin Maronde <myemailisjustin@me.com>
2022-12-25 12:36:58 -0500

    call openai if no image provided

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -5,6 +5,11 @@ var Comment = mongoose.model("Comment");
 var User = mongoose.model("User");
 var auth = require("../auth");
 const { sendEvent } = require("../../lib/event");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 // Preload item objects on routes with ':item'
 router.param("item", function(req, res, next, slug) {
@@ -139,12 +144,21 @@ router.get("/feed", auth.required, function(req, res, next) {
 
 router.post("/", auth.required, function(req, res, next) {
   User.findById(req.payload.id)
-    .then(function(user) {
+    .then(async function(user) {
       if (!user) {
         return res.sendStatus(401);
       }
 
       var item = new Item(req.body.item);
+
+      if (req.body.image === '' || req.body.image === undefined) {
+        const response = await openai.createImage({
+          prompt: req.body.item.title,
+          n: 1,
+          size: "256x256",
+        });
+        item.image = response.data.data[0].url;
+      }
 
       item.seller = user;
 
